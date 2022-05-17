fcce6ff025bccb4aed2bca59a8ea6f6738aa47c7
ran65132 <r65132@qq.com>
2022-05-17 17:54:14 +0800

    练习

--- 100~200 prime number/源.c
+++ 100~200 prime number/源.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 int main()
 {
+	int a = 100;
+	int b;
+	while (a < 200)
+	{
+		a++;
+		b = 1;
+		while (b < a)
+		{
+			b++;
+			if (a % b == 0)
+			{
+				break;
+			}
+		}
+		if (a == b)
+		{
+			printf("%d\n", a);
+		}
+	}
+
 	return 0;
 }
